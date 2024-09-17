c1805d5fadf0cda06f07c967e2da56562ad5eeb9
Vivek Pillai <103922350+Vivek7083@users.noreply.github.com>
2024-09-17 23:28:26 +0530

    Update Navbar.jsx
    Links updated

--- portfolio-app/src/components/Navbar.jsx
+++ portfolio-app/src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import logo from '../assets/kevinRushLogo.png'
+import 
 import {FaLinkedin, FaXTwitter, FaInstagram, FaGithub} from 'react-icons/fa6'
 
 export default function Navbar(){
@@ -9,10 +10,18 @@ export default function Navbar(){
                 <img className='mx-2 w-10' src={logo} alt="Logo"/>
             </div>
             <div className='m-8 flex items-center justify-center gap-4 text-2xl'>
-                <FaLinkedin/>
-                <FaGithub/>
-                <FaInstagram/>
-                <FaXTwitter/>
+                <a href="https://www.linkedin.com/in/vivek-pillai-9340b923a" target="_blank" rel="noopener noreferrer">
+                    <FaLinkedin />
+                </a>
+                <a href="https://github.com/Vivek7083" target="_blank" rel="noopener noreferrer">
+                    <FaGithub />
+                </a>
+              <a href="https://instagram.com/vivekvpillai_" target="_blank" rel="noopener noreferrer">
+                <FaInstagram />
+              </a>
+              <a href="https://x.com/VivkPillai" target="_blank" rel="noopener noreferrer">
+                <FaTwitter />
+              </a>
             </div>
         </nav>
         </>
