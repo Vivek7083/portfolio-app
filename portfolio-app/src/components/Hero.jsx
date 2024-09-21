e37b8b60c418cd835aaff033be34f26d5f633ace
Vivek Pillai <103922350+Vivek7083@users.noreply.github.com>
2024-09-21 15:22:37 +0530

    Update Hero.jsx

--- portfolio-app/src/components/Hero.jsx
+++ portfolio-app/src/components/Hero.jsx
@@ -1,5 +1,5 @@
 import { HERO_CONTENT } from "../constants";
-import profilePic from "../assets/kevinRushProfile.png";
+import profilePic from "../assets/my_pic_onam.jpg";
 import { motion } from "framer-motion";
 import { useRef } from "react";
 
